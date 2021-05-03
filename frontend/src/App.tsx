300bb62143165698d8c2688a600eefe494cc98b4
Denis Levy <denis.levi11@gmail.com>
2021-05-03 14:21:21 -0600

    Added bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <h1 className="text-primary">Hello, world!</h1>
     </div>
   );
 }
